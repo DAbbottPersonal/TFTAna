81bffb48d8ae85c6943665cd01ceae7ea933e1eb
DAbbottPersonal <DaleAbbott999@gmail.com>
2021-02-16 13:18:57 -0500

    Case fixes

--- Source/AnalyzeSynergies.py
+++
@@ -1,8 +0,0 @@
-#########################################################
-# Discover some fun facts about perfect synergies! ######
-#########################################################
-import io
-from yaml import safe_load, dump
-
-
-
--- Source/test_yaml.py
+++
@@ -1,9 +0,0 @@
-from yaml import safe_load, dump
-
-champion_data = None
-with open("../Data/Champions.yaml", 'r') as stream:
-    champion_data = safe_load(stream)
-
-print( champion_data )
-
-
